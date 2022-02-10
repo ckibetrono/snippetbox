f1870ddcf18a6ac27e6bd48290a838d68306208f
Kibet Rono <ckibetrono@gmail.com>
2022-02-10 17:28:25 +0300

    changes to cmd/web/

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -47,8 +47,9 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
   if err != nil {
     // Also update the code here to use the error logger from the application 
     // struct 
-    app.errorLog.Println(err.Error())
-    http.Error(w, "Internal Server Error", 500)
+    // app.errorLog.Println(err.Error())
+    //http.Error(w, "Internal Server Error", 500)
+    app.serverError(w, err) // Use the serverError() helper   
   }
 
 
@@ -61,7 +62,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-		http.NotFound(w, r)
+		app.notFound(w) // Use the notFound() helper 
 		return
 	}
 
@@ -73,8 +74,9 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-		http.Error(w, "Method Not Allowed", 405)
-		return
+		// http.Error(w, "Method Not Allowed", 405)
+		app.clientError(w, http.StatusMethodNotAllowed) // use the clientError() helper 
+    return
 	}
 
 	w.Write([]byte("Create a new snippet..."))
